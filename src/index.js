f92a02451f1a05943e17385535ab3c540d492090
LeonardoJaques <jaques.projetos@outlook.com>
2021-04-02 20:08:24 -0300

    Add first loaders to handle CSS

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
+import { run } from './app/app';
+import './main.css';
 import { AlertService } from './app/alert.service';
 import { ComponentService } from './app/component.service';
-import { run } from './app/app';
 
 const alertService = new AlertService();
 const componentService = new ComponentService();
